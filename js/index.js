67c2426b3b9c93fea67fc186dad9552a9c1762cb
Tlacaelel Leon <radeon777@live.com>
2020-09-16 11:42:49 -0600

    Added events to menu links

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 //bootstrap
+window.scrollTo(0,0);
 $(function () {
     $('[data-toggle="popover"]').popover()
     $('[data-toggle="tooltip"]').tooltip()
 })
+
 //IMPORTS
 import { navTransition } from './transitions.js';
 import observer from './observer.js';
 let mainObserver = observer();
 //DOM
 let nav = document.getElementById('nav')
+let body = document.getElementsByTagName('body')[0]
 let navLinks = document.getElementsByClassName('nav-link');
 let aboutText = document.getElementById('aboutText');
 let aboutImage = document.getElementById('aboutImage');
@@ -23,10 +26,37 @@ let skill6 = document.querySelector('#skill6');
 let project1 = document.querySelector('#project1');
 let project2 = document.querySelector('#project2');
 let project3 = document.querySelector('#project3');
+let aboutMenu = document.querySelector('#aboutMenu');
+let skillsMenu = document.querySelector('#skillsMenu');
+let projectsMenu = document.querySelector('#projectsMenu');
+let home = document.querySelector('#home');
 let container = nav.children[0];
 //addEvent to window
 window.onscroll = navTransition(nav, container, navLinks);
-
+aboutMenu.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,aboutText.offsetTop-(window.innerHeight/3));
+}
+skillsMenu.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,skill1.offsetTop-(window.innerHeight/3));
+}
+projectsMenu.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,project1.offsetTop-(window.innerHeight/4));
+}
+projectsMenu.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,project1.offsetTop-(window.innerHeight/4));
+}
+projectsMenu.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,project1.offsetTop-(window.innerHeight/4));
+}
+home.onclick= (e) => {
+    e.preventDefault();
+    window.scrollTo(0,0);
+}
 
 mainObserver.observe(aboutText);
 mainObserver.observe(aboutImage);
