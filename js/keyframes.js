e59921efb1c8f0cca9d880ca73b63cb81c530a3e
Tlacaelel Leon <radeon777@live.com>
2020-09-15 19:16:43 -0600

    animating contact

--- js/keyframes.js
+++ js/keyframes.js
@@ -1,4 +1,14 @@
 let keyframes = (id) =>{
+    let contact = [
+      {
+        transform: 'scale(.8)',
+        opacity:0
+      },
+      {
+        transform: 'scale(1)',
+        opacity:1
+      }
+    ];
     let options ={
       aboutText: [
         {
@@ -21,16 +31,8 @@ let keyframes = (id) =>{
             opacity: 1
         }
       ],
-      contactTitle: [
-        {
-          transform: 'translateX(200px)',
-          opacity:0
-        },
-        {
-          transform: 'translateX(0)',
-          opacity:1
-        }
-      ]
+      contactTitle: contact,
+      contactButton: contact
   }
   return options[id];
 }
