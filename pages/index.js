21de5e189d5e945a7708b3c1009e43f3766430f8
Tlacaelel <radeon777@live.com>
2021-06-16 11:22:05 -0600

    Separated the logic of getting locales

--- pages/index.js
+++ pages/index.js
@@ -4,53 +4,11 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import getT from "next-translate/getT";
+import { getLocales } from "../utils";
 import Head from "next/head";
 
 export async function getStaticProps({ locale, locales }) {
-  const switchBtn = locales.filter((i) => i !== locale);
-  const t = await getT(locale, "home");
-  const switchLang =
-    switchBtn[0] === "en" ? "English" : switchBtn[0] === "es" ? "Español" : "";
-
-  const menubar = {
-    about: t("about"),
-    skills: t("skills"),
-    projects: t("projects"),
-    switchLang,
-    switchBtn: switchBtn[0] === "en" ? [""] : switchBtn,
-  };
-  const hero = {
-    webDev: t("webDev"),
-    title: t("title"),
-    message: t("message"),
-  };
-  const about = {
-    title: t("titleAbout"),
-    paragraph: t("paragraphAbout"),
-    contact: t("contactAbout"),
-    subject: t("subjectAbout"),
-  };
-
-  const projects = {
-    title: t("projectTitle"),
-    projectsList: t("projectsList", { count: 1 }, { returnObjects: true }),
-    projectVisit: t("projectVisit"),
-    projectCode: t("projectCode"),
-  };
-  const skills = {
-    title: t("skillsTitle"),
-    skillsList: t("skillsList", { count: 1 }, { returnObjects: true }),
-  };
-  return {
-    props: {
-      menubar,
-      hero,
-      about,
-      projects,
-      skills,
-    },
-  };
+  return await getLocales(locale, locales);
 }
 export default function Home(props) {
   return (
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,52 @@
+import getT from "next-translate/getT";
+
 export const isVertical = (title) => {
   if (title === "Nutrition cooking") return true;
   return false;
+};
+
+export const getLocales = async (locale, locales) => {
+  const switchBtn = locales.filter((i) => i !== locale);
+  const t = await getT(locale, "home");
+  const switchLang =
+    switchBtn[0] === "en" ? "English" : switchBtn[0] === "es" ? "Español" : "";
+
+  const menubar = {
+    about: t("about"),
+    skills: t("skills"),
+    projects: t("projects"),
+    switchLang,
+    switchBtn: switchBtn[0] === "en" ? [""] : switchBtn,
+  };
+  const hero = {
+    webDev: t("webDev"),
+    title: t("title"),
+    message: t("message"),
+  };
+  const about = {
+    title: t("titleAbout"),
+    paragraph: t("paragraphAbout"),
+    contact: t("contactAbout"),
+    subject: t("subjectAbout"),
+  };
+
+  const projects = {
+    title: t("projectTitle"),
+    projectsList: t("projectsList", { count: 1 }, { returnObjects: true }),
+    projectVisit: t("projectVisit"),
+    projectCode: t("projectCode"),
+  };
+  const skills = {
+    title: t("skillsTitle"),
+    skillsList: t("skillsList", { count: 1 }, { returnObjects: true }),
+  };
+  return {
+    props: {
+      menubar,
+      hero,
+      about,
+      projects,
+      skills,
+    },
+  };
 };
